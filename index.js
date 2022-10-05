d5665386208575380e73817b3caba8c1dae91383
Mikhail Varushichev <Mikeloangel@gmail.com>
2022-10-05 15:25:30 +0300

    adds burger menu

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import SearchBox from "./blocks/search-box/search-box.js";
 import enableLangSelect from "./blocks/lang/lang.js";
+import Menu from "./blocks/menu/menu.js";
 
 const search = new SearchBox({boxSelector: 'search-box'});
 enableLangSelect();
+const menu = new Menu({})
 
 
 
